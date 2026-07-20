857cdd420e492ed9450cb2c5b339ef5ab25dff53
jherrera3131 <jaironh31@gmail.com>
2026-07-20 14:38:27 -0400

    Update database connection password handling

--- db_final_demo.py
+++ db_final_demo.py
@@ -9,7 +9,7 @@ def create_connection():
         connection = mysql.connector.connect(
             host='127.0.0.1',
             user='root',
-            password='Jabooze_31',
+            password= acc_password,
             database='homedepot'
         )
         return connection
